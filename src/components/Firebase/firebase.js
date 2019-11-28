2cb874bbcfd86248d113bcdf910588bf5f0c5e28
karan1907 <goldyking11@gmail.com>
2019-11-28 11:04:05 +0530

    Completed sign up facility with email and password using firebase api

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,4 +1,5 @@
 import app from 'firebase/app';
+import 'firebase/auth';
 
 const config = {
     apiKey: "AIzaSyAjOoiiKx88S8BHd5MbFrTAc1ZqbSqY2S4",
@@ -14,7 +15,21 @@ const config = {
 class Firebase{
     constructor(){
         app.initializeApp(config);
+
+        this.auth = app.auth();
     }
+    
+    // ********** Auth API ************ //
+
+    doCreateUserWithEmailAndPassword = (email,password) => this.auth.createUserWithEmailAndPassword(email,password);
+
+    doSignInWithEmailAndPassword = (email,password) => this.auth.signInWithEmailAndPassword(email,password);
+
+    doSignOut = () => this.auth.signOut();
+
+    doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
+
+    doPasswordUpdate = password => this.auth.currentUser.updatePassword(password);
 }
 
 export default Firebase;
